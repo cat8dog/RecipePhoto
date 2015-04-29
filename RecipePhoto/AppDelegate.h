37e63aca40bcbe96ae322e6af0d178844bc6921a
cat8dog <catherine.reyto@gmail.com>
2015-04-29 16:08:09 -0400

    Initial Commit

---
+++ RecipePhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecipePhoto
+//
+//  Created by Catherine Reyto on 2015-04-29.
+//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecipePhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecipePhoto
+//
+//  Created by Catherine Reyto on 2015-04-29.
+//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
